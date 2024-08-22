08ed25764d88bbc8a92218133c4cd7d97ac73b9c
Diego <d.cataldo120@gmail.com>
2024-08-22 09:50:54 -0400

    cc

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,16 +1,16 @@
 //START PRODUCTION
-/*
+
 export const BASE_URL = 'https://dailybackend-fbbhdydwe9g7emhk.eastus-01.azurewebsites.net/api'
-*/
+
 
 //end development
 
 //START DEVELOPMENT 
-
+/*
 export const BASE_URL = 'http://127.0.0.1:8000/api'
 
 //END DEVELOPMENT
-
+*/
 export const getConfig = (token) => {
     const config = {
         headers: {
